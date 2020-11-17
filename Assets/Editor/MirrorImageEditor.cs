aa01f1b1905bee43bbea60f5ec716814deea4302
linxinfa <37791251+linxinfa@users.noreply.github.com>
2020-11-17 19:03:05 +0800

    Unity UGUI Mirror Image

---
+++ Assets/Editor/MirrorImageEditor.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEngine.UI;
+using UnityEditor.UI;
+
+[CustomEditor(typeof(MirrorImage))]
+public class MirrorImageEditor:ImageEditor
+{
+    SerializedProperty m_mirrorType;
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+
+        EditorGUILayout.PropertyField(m_mirrorType);
+        serializedObject.ApplyModifiedProperties();
+    }
+   
+
+    protected override void OnEnable()
+    {
+        base.OnEnable();
+        m_mirrorType = serializedObject.FindProperty("m_mirrorType");
+        
+    }
+}
+
